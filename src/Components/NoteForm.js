bc00873c027f0604aef8ab4bc64807f916110c12
122208997 <lehaohust@gmail.com>
2018-11-24 17:01:48 +0700

    get whatever user type from form

--- src/Components/NoteForm.js
+++ src/Components/NoteForm.js
@@ -1,6 +1,26 @@
 import React, { Component } from 'react';
 
 class NoteForm extends Component {
+    constructor(props) {
+        super(props);
+        
+    }
+    
+    isChange = (event) => {
+        var name = event.target.name;
+        var value =  event.target.value;
+        this.setState({
+            [name] : value
+        });   
+    }
+
+    addData = (title, content) => {
+        var item = {};
+        item.title = title;
+        item.content = content;
+        console.log(item);  
+    }
+
     render() {
         return (
             <div className="col-4">
@@ -8,13 +28,19 @@ class NoteForm extends Component {
                     <h3>Edit Note</h3>
                     <div className="form-group">
                         <label htmlFor="title">Title</label>
-                        <input type="text" name="title" id="title" className="form-control"/>
+                        <input type="text" name="title" id="title" className="form-control"
+                           onChange= { (event) => this.isChange(event)} 
+                        />
                     </div>
                     <div className="form-group">
                         <label htmlFor="content">Content</label>
-                        <input type="text" name="content" id="content" className="form-control"/>
+                        <input type="text" name="content" id="content" className="form-control"
+                            onChange= { (event) => this.isChange(event)} 
+                        />
                     </div>
-                    <input type="reset" value="Add" className="btn btn-block btn-primary"/>
+                    <input type="reset" value="Add" className="btn btn-block btn-primary"
+                        onClick={ (title, content) => this.addData(this.state.title, this.state.content)}
+                    />
                 </form>
             </div>
         );
